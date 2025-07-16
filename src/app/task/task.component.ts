17a26ae93d782e14443a247d076f2a799d2e391b
Asim Ehab <aehab@sumerge.com>
2025-07-16 16:35:57 +0300

    fixing some style issues

--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -1,11 +1,12 @@
 import { Component, input, signal, computed } from '@angular/core';
 import { todo } from '../tasks-columns/todo.model';
 import { TasksService } from '../tasks-columns/tasks.service';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-task',
   standalone: true,
-  imports: [],
+  imports: [MatProgressSpinnerModule],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css',
 })
@@ -13,13 +14,16 @@ export class TaskComponent {
   data = input.required<todo>();
   constructor(private tasksService: TasksService) {}
   // isChecked = computed(() => this.data().status === 'completed');
+  isLoading = signal(false);
 
   deleteTodo() {
+    this.isLoading.set(true);
     this.tasksService.deleteTodo(this.data().id).subscribe({
       next: () => {
         this.tasksService.tasks.update((todos) =>
           todos.filter((todo) => todo.id !== this.data().id)
         );
+        this.isLoading.set(false);
       },
       error: (error) => {
         console.error('Error deleting todo:', error);
@@ -35,7 +39,7 @@ export class TaskComponent {
       ...this.data(),
       status: checkedd ? 'completed' : 'pending',
     };
-
+    this.isLoading.set(true);
     this.tasksService
       .updateTodoStatus(this.data().id, updatedTodo.status)
       .subscribe({
@@ -45,6 +49,7 @@ export class TaskComponent {
               todo.id == this.data().id ? updatedTodo : todo
             )
           );
+          this.isLoading.set(false);
         },
         error: (error) => {
           console.error('Error updating todo status:', error);
@@ -53,5 +58,7 @@ export class TaskComponent {
           console.log('Todo status updated successfully');
         },
       });
+    
   }
+      
 }
