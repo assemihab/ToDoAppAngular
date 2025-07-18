ebc7748cbb3d3f6abaff322af954b3aedf631520
Asim Ehab <assimehab@gmail.com>
2025-07-18 18:20:20 +0300

    adding todos to fix error

--- src/app/mock/partialMockTasksServce.service.ts
+++ src/app/mock/partialMockTasksServce.service.ts
@@ -15,5 +15,8 @@ export class PartialMockTasksService extends TasksService {
     { id: '1', name: 'Mock Task 1', priority: 1, status: 'pending' },
     { id: '2', name: 'Mock Task 2', priority: 2, status: 'completed' },
     { id: '3', name: 'Mock Task 3', priority: 3, status: 'pending' },
+    { id: '4', name: 'Mock Task 4', priority: 1, status: 'completed' },
+    { id: '5', name: 'Mock Task 5', priority: 1, status: 'pending' },
+    {id: '6', name: 'Mock Task 6', priority: 1, status: 'completed' },
   ]);
 }
